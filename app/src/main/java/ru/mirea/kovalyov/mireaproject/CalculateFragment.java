18917a8e10273264a148eb8e789d3641c73abfa6
kovalyov-z3 <71659839+kovalyov-z3@users.noreply.github.com>
2022-05-02 21:52:26 +0300

    Created project with Calculator and Browser

---
+++ app/src/main/java/ru/mirea/kovalyov/mireaproject/CalculateFragment.java
@@ -0,0 +1,57 @@
+package ru.mirea.kovalyov.mireaproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import ru.mirea.kovalyov.mireaproject.R;
+
+public class CalculateFragment extends Fragment {
+    private EditText editText;
+    private TextView textView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calculate, container, false);
+        view.findViewById(R.id.calculateButton).setOnClickListener(this::onClickCalculate);
+        editText = view.findViewById(R.id.editTextCalculate);
+        textView = view.findViewById(R.id.textViewResult);
+        return view;
+    }
+
+    public void onClickCalculate(View view) {
+        String input = editText.getText().toString();
+        String[] arr = input.split(" ");
+        double result = 0;
+
+        try {
+            double x = Double.parseDouble(arr[0]);
+            double y = Double.parseDouble(arr[2]);
+            switch (arr[1]) {
+                case "+":
+                    result = x + y;
+                    break;
+                case "-":
+                    result = x - y;
+                    break;
+                case "*":
+                    result = x * y;
+                    break;
+                case "/":
+                    result = x / y;
+                    break;
+            }
+
+            textView.setText(Double.toString(result));
+        } catch (Exception e) {
+            textView.setText("Вы ввели некорректное выражение");
+        }
+    }
+}
---
+++ app/src/main/java/ru/mirea/kovalyov/mireaproject/WebFragment.java
@@ -0,0 +1,38 @@
+package ru.mirea.kovalyov.mireaproject;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+import ru.mirea.kovalyov.mireaproject.R;
+
+
+public class WebFragment extends Fragment {
+    private WebView webView;
+    private EditText searchInput;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_web, container, false);
+        view.findViewById(R.id.search_button).setOnClickListener(this::onSearchButtonClick);
+
+        searchInput = (EditText) view.findViewById(R.id.url_input);
+        webView = (WebView) view.findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.google.com/");
+        return view;
+    }
+
+    private void onSearchButtonClick(View view){
+        String searchText = "https://"+ searchInput.getText().toString();
+        webView.loadUrl(searchText);
+    }
+}
